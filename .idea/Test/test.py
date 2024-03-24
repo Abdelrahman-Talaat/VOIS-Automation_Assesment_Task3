23d1b06c125011fe18d34aa952be6bcbc5df6acb
Abdelrahman <abdelrahmantalaat974@gmail.com>
2024-03-24 22:34:23 +0200

    update the project

--- .idea/Test/test.py
+++ .idea/Test/test.py
@@ -5,16 +5,16 @@ import pandas as pd
 # Load the data into a DataFrame
 df = pd.read_csv('Employees.csv')
 
-# 1. Remove duplicates
+#  Remove duplicates
 df = df.drop_duplicates()
 
-# 2. Remove decimal places in the Age column
+#  Remove decimal places in the Age column
 df['Age'] = df['Age'].astype(int)
 
-# 3. Convert USD salary to EGP (assuming 1 USD = 15 EGP)
+#  Convert USD salary to EGP (assuming 1 USD = 15 EGP)
 df['Salary(USD)'] = df['Salary(USD)'] * 55
 
-# 4. Print statistics
+#  Print statistics
 print("Average age:", df['Age'].mean())
 print("Median salary:", df['Salary(USD)'].median())
 gender_ratio = df['Gender'].value_counts(normalize=True)
@@ -23,6 +23,6 @@ print("Male:", gender_ratio['M'])
 print("Female:", gender_ratio['F'])
 
 
-# 5. Write data to a new CSV file
+#  Write data to a new CSV file
 df.to_csv('Filtered_data.csv', index=False)
 
