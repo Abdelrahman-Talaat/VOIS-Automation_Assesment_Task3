06bc5dd3060318fbc7bf8f71dff24975a0ffb8c8
Abdelrahman <abdelrahmantalaat974@gmail.com>
2024-03-25 17:27:22 +0200

    update the project

--- .idea/Test/Test.py
+++ .idea/Test/Test.py
@@ -11,7 +11,7 @@ df = df.drop_duplicates()
 #  Remove decimal places in the Age column
 df['Age'] = df['Age'].astype(int)
 
-#  Convert USD salary to EGP (assuming 1 USD = 15 EGP)
+#  Convert USD salary to EGP (assuming 1 USD = 55 EGP)
 df['Salary(USD)'] = df['Salary(USD)'] * 55
 
 #  Print statistics
@@ -21,7 +21,6 @@ gender_ratio = df['Gender'].value_counts(normalize=True)
 print("Ratio between males and female employees:")
 print("Male:", gender_ratio['M'])
 print("Female:", gender_ratio['F'])
-
 
 #  Write data to a new CSV file
 df.to_csv('Filtered_data.csv', index=False)
---
+++ README.md
@@ -0,0 +1,17 @@
+#  VOIS-Automation_Assesment_Task3
+
+## Introduction
+
+This project includes automated test scripts for the below VOIS assesment task:
+## Task Description:
+### Scenario:
+Load the data inside the provided CSV file into a Data Frame and perform the following:
+- Remove any duplicates in the table.
+- Convert the USD salary to EGP
+- Print in the console some stats like:
+	- Average age
+	- Median Salaries
+	- Ration between males and female employees
+
+
+- After performing the previous steps, write the data in a new CSV file.
